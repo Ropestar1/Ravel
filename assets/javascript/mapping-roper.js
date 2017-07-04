c7f85633d80c415230b7b7ac8a1c24712595ec26
HQ <Spawn@ROPERs-MacBook-Pro.local>
2017-07-04 01:15:22 -0700

    Working on getting map and routes to work

--- assets/javascript/mapping-roper.js
+++ assets/javascript/mapping-roper.js
@@ -1,46 +1,90 @@
+// starting coordinates
+var startLat;
+var startLng;
 
-$(document).ready(function () {
+// ending coordinates
+var endLat;
+var endLng;
+
+// google documentation had these added
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
+// when submit button is clicked
 $("#submit").on("click", function(event){
+	event.preventDefault();
 
- event.preventDefault();
+	// Creates an area to put the googlemaps results
+	$('div.map-results-area').append('<div class="panel panel-primary" id="map-header-holder"></div>');
+	$('#map-header-holder').html('<div class="panel-heading panel-heading-ravel" id="map-header-text"></div>');
+	$('#map-header-text').html('<h3 class="panel-title"><strong> Map Routes </strong></h3>');
 
- var destination = $("#destination").val().trim();
- var origin = $("#origin").val().trim();
- // var time = $("#time").val().trim();
-  var queryURL = 
- "https://cors-anywhere.herokuapp.com/"+"https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&Mode=BICYCLING&key=AIzaSyA0oMhk60GOJSoK46J8f0dAwghkBwgThl0";
+	$('#map-header-holder').append('<div class="panel-body" id="map-body"></div>');
 
+	var destination = $("#destination").val().trim();
+	var origin = $("#origin").val().trim();
+	 // var time = $("#time").val().trim();
+	var queryURL = 
+		"https://cors-anywhere.herokuapp.com/"+
+		"https://maps.googleapis.com/maps/api/directions/json?origin=" + 
+		origin + "&destination=" + destination + 
+		"&Mode=BICYCLING&key=AIzaSyA0oMhk60GOJSoK46J8f0dAwghkBwgThl0";
 
+			// "https://maps.googleapis.com/maps/api/directions/json?&mode=BICYCLING&origin=sunnyvale&destination=cupertino&departure_time=" + time +  "&key=AIzaSyA0oMhk60GOJSoK46J8f0dAwghkBwgThl0"
 
+	console.log(queryURL);
+	
+	$.ajax({
+		url : queryURL,
+		method : "GET",
+		dataType : 'json'
 
+	}).done(function(response){
+		// debugger;
+		startLat = response.routes[0].legs[0].start_location.lat;
+		startLng = response.routes[0].legs[0].start_location.lng;
 
- // "https://maps.googleapis.com/maps/api/directions/json?&mode=BICYCLING&origin=sunnyvale&destination=cupertino&departure_time=" + time +  "&key=AIzaSyA0oMhk60GOJSoK46J8f0dAwghkBwgThl0"
+		endLat = response.routes[0].legs[0].end_location.lat;
+		endLng = response.routes[0].legs[0].end_location.lng;
 
-  console.log(queryURL);
-  $.ajax({
-    url : queryURL,
-    method : "GET",
-    dataType : 'json'
-   
- }).done(function(response){
- 	// debugger;
-   console.log("done : "+ response);
-   // $("#map").append(response);
-
-})
-
+		// $("#map").append(response);
+		initialize();
+		calcRoute();
+	});
 });
 
 
+// working on googlemaps api call
 
+function initialize() {
+	directionsDisplay = new google.maps.DirectionsRenderer();
+	var startPoint = new google.maps.LatLng(startLat, startLng);
+	var mapOptions = {
+		zoom: 10,
+		center: startPoint,
+	}
 
-});
+	//might need to fix line below to take our user input. fix the document.getElementByID
+	map = new google.maps.Map(document.getElementById('map-body'), mapOptions);
+	directionsDisplay.setMap(map);
+}
 
+function calcRoute() {
+  //origin already set in variable on click. see origin
+  	// var start = document.getElementById('start').value;
+  // destination already set in variable on click. see destination
+  	// var end = document.getElementById('end').value;
+  
+  var request = {
+    origin: origin, //might want to use a different variable name
+    destination: destination, //might want to use a different variable name
+    travelMode: 'BICYCLING'
+  };
 
-
-
-
-
-
-
-
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+    }
+  });
+}
