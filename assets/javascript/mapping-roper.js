dfe65d928122fcde11ab78e9399c3e6530f8cc9b
HQ <Spawn@calvisitor-10-105-171-82.calvisitor.1918.berkeley.edu>
2017-07-05 16:27:15 -0700

    upating

--- assets/javascript/mapping-roper.js
+++ assets/javascript/mapping-roper.js
@@ -13,67 +13,67 @@ var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
 
+
 // when submit button is clicked
 $("#submit").on("click", function(event){
-	event.preventDefault();
+    event.preventDefault();
 
-	// Creates an area to put the googlemaps results
-	$('div.map-results-area').html('<div class="panel panel-primary" id="map-header-holder"></div>');
-	$('#map-header-holder').html('<div class="panel-heading panel-heading-ravel" id="map-header-text"></div>');
-	$('#map-header-text').html('<h3 class="panel-title"><strong> Map Routes </strong></h3>');
+    // Creates an area to put the googlemaps results
+    // $('div.map-results-area').html('<div class="panel panel-primary" id="map-header-holder"></div>');
+    // $('#map-header-holder').html('<div class="panel-heading panel-heading-ravel" id="map-header-text"></div>');
+    // $('#map-header-text').html('<h3 class="panel-title"><strong> Map Routes </strong></h3>');
 
-	$('#map-header-holder').append('<div class="panel-body" id="map-body"></div>');
+    // $('#map-header-holder').append('<div class="panel-body" id="map-body"></div>');
 
-	origin = $("#origin").val().trim();
-	destination = $("#destination").val().trim();
-	 // var time = $("#time").val().trim();
-	var queryURL = 
-		"https://cors-anywhere.herokuapp.com/"+
-		"https://maps.googleapis.com/maps/api/directions/json?origin=" + 
-		origin + "&destination=" + destination + 
-		"&Mode=BICYCLING&key=AIzaSyA0oMhk60GOJSoK46J8f0dAwghkBwgThl0";
+    origin = $("#origin").val().trim();
+    destination = $("#destination").val().trim();
+     // var time = $("#time").val().trim();
+    var queryURL = 
+        "https://cors-anywhere.herokuapp.com/"+
+        "https://maps.googleapis.com/maps/api/directions/json?origin=" + 
+        origin + "&destination=" + destination + 
+        "&Mode=BICYCLING&key=AIzaSyA0oMhk60GOJSoK46J8f0dAwghkBwgThl0";
 
-			// "https://maps.googleapis.com/maps/api/directions/json?&mode=BICYCLING&origin=sunnyvale&destination=cupertino&departure_time=" + time +  "&key=AIzaSyA0oMhk60GOJSoK46J8f0dAwghkBwgThl0"
+            // "https://maps.googleapis.com/maps/api/directions/json?&mode=BICYCLING&origin=sunnyvale&destination=cupertino&departure_time=" + time +  "&key=AIzaSyA0oMhk60GOJSoK46J8f0dAwghkBwgThl0"
 
-	console.log(queryURL);
-	
-	$.ajax({
-		url : queryURL,
-		method : "GET",
-		dataType : 'json'
+    console.log(queryURL);
+    
+    $.ajax({
+        url : queryURL,
+        method : "GET",
+        dataType : 'json'
 
-	}).done(function(response){
-		// debugger;
-		startLat = response.routes[0].legs[0].start_location.lat;
-		startLng = response.routes[0].legs[0].start_location.lng;
+    }).done(function(response){
+        // debugger;
+        startLat = response.routes[0].legs[0].start_location.lat;
+        startLng = response.routes[0].legs[0].start_location.lng;
 
-		endLat = response.routes[0].legs[0].end_location.lat;
-		endLng = response.routes[0].legs[0].end_location.lng;
+        endLat = response.routes[0].legs[0].end_location.lat;
+        endLng = response.routes[0].legs[0].end_location.lng;
 
-		// $("#map").append(response);
-		initialize();
-		calcRoute();
-	});
+        calcRoute();
+    });
 });
 
-function initialize() {
-	directionsDisplay = new google.maps.DirectionsRenderer();
-	var ucbe = new google.maps.LatLng(37.7918156, -122.3931774);
-	var mapOptions = {
-		zoom: 10,
-		center: ucbe,
-	}
 
-	//might need to fix line below to take our user input. fix the document.getElementByID
-	map = new google.maps.Map(document.getElementById('map-body'), mapOptions);
-	directionsDisplay.setMap(map);
+function initialize() {
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var ucbe = new google.maps.LatLng(37.7918156, -122.3931774);
+    var mapOptions = {
+        zoom: 10,
+        center: ucbe,
+    }
+
+    //might need to fix line below to take our user input. fix the document.getElementByID
+    map = new google.maps.Map(document.getElementById('map-body'), mapOptions);
+    directionsDisplay.setMap(map);
 }
 
 function calcRoute() {
   //origin already set in variable on click. see origin
-  	// var start = document.getElementById('start').value;
+    // var start = document.getElementById('start').value;
   // destination already set in variable on click. see destination
-  	// var end = document.getElementById('end').value;
+    // var end = document.getElementById('end').value;
   
   var request = {
     origin: origin, //might want to use a different variable name
@@ -86,4 +86,8 @@ function calcRoute() {
       directionsDisplay.setDirections(result);
     }
   });
+
+  directionsDisplay.setPanel(document.getElementById('right-panel'))
 }
+
+initialize();
