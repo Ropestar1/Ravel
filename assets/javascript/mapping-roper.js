750c7b2d9dbae015a9f5121a89437d352cb5e22c
HQ <Spawn@ROPERs-MacBook-Pro.local>
2017-07-05 22:15:06 -0700

    working firebase test. need to change back to Samya firebase

--- assets/javascript/mapping-roper.js
+++ assets/javascript/mapping-roper.js
@@ -1,3 +1,22 @@
+// FIREBASE
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBGKxL60DKgOYk5ZXDQZuv_qJ6h_ptZaf0",
+    authDomain: "ravel-test-aadbc.firebaseapp.com",
+    databaseURL: "https://ravel-test-aadbc.firebaseio.com",
+    projectId: "ravel-test-aadbc",
+    storageBucket: "ravel-test-aadbc.appspot.com",
+    messagingSenderId: "539214141426"
+};
+
+firebase.initializeApp(config);
+
+// FIREBASE CUSTOMIZATIONS
+var database = firebase.database();
+var routeName;
+var username;
+// FIREBASE Variables
+
 // starting coordinates
 var origin;
 var startLat;
@@ -13,28 +32,57 @@ var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
 
+database.ref().on("value", function(snapshot) {
+	console.log("this is the snapshot: " + snapshot)
+	console.log(snapshot)
+	// Print the initial data to the console.
+	console.log(snapshot.val());
+	// Log the value of the various properties
+	// console.log(snapshot.val().username);
+	// console.log(snapshot.val().routeName);
+	// console.log(snapshot.val().distanceTravelled);
+	// console.log(snapshot.val().time);
+	// console.log(snapshot.val().speed);
+
+
+	// If any errors are experienced, log them to console.
+	}, function(errorObject) {
+		console.log("The read failed: " + errorObject.code);
+});
 
 // when submit button is clicked
 $("#submit").on("click", function(event){
     event.preventDefault();
 
-    // Creates an area to put the googlemaps results
-    // $('div.map-results-area').html('<div class="panel panel-primary" id="map-header-holder"></div>');
-    // $('#map-header-holder').html('<div class="panel-heading panel-heading-ravel" id="map-header-text"></div>');
-    // $('#map-header-text').html('<h3 class="panel-title"><strong> Map Routes </strong></h3>');
-
-    // $('#map-header-holder').append('<div class="panel-body" id="map-body"></div>');
-
+    username = 'Roper'; //$("#name").val().trim();
+    
+    routeName = $("#route-name").val().trim();
+    // console.log("username is : "+ username);
+    // console.log("routeName is : "+ routeName);
     origin = $("#origin").val().trim();
     destination = $("#destination").val().trim();
-     // var time = $("#time").val().trim();
+    
+    database.ref('/' + username).set({
+	    // username: username,
+	    namedRoute: routeName,
+	    distanceTravelled : 5000, //distance,
+	    timeTaken : 1000, //time,
+	    // speed : speed
+  	});
+
+  	// Change the HTML
+	// var row = $("<tr>");
+	// row.append("<td>" +  snapshot.val().routeName + "</td>")
+	// row.append("<td>" +  snapshot.val().distanceTravelled + "</td>");
+	// row.append("<td>" +  snapshot.val().speed + "</td>");
+	// row.append("<td>" +  snapshot.val().time + "</td>");
+
+    //GOOGLEMAPS API CALLS
     var queryURL = 
         "https://cors-anywhere.herokuapp.com/"+
         "https://maps.googleapis.com/maps/api/directions/json?origin=" + 
         origin + "&destination=" + destination + 
         "&Mode=BICYCLING&key=AIzaSyA0oMhk60GOJSoK46J8f0dAwghkBwgThl0";
-
-            // "https://maps.googleapis.com/maps/api/directions/json?&mode=BICYCLING&origin=sunnyvale&destination=cupertino&departure_time=" + time +  "&key=AIzaSyA0oMhk60GOJSoK46J8f0dAwghkBwgThl0"
 
     console.log(queryURL);
     
